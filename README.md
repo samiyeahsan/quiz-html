6154678216a500866c4ff599890c922c4fab74e8
Samiye Ahsan <samiyeahsann88@gmail.com>
2023-10-11 10:08:54 -0500

    firs commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# This is a general quiz
+this quiz will ask some general questions
+
+## Start button
+user will hit  the start button and questions will start and timer will start and every time user answer wrong it will deduct 5 second from time and it will go to next quesion and when all quesions answered it will display quiz end and shows the score.
+
+
+### The following image shows the web application's appearance and functionality:
+![preview image](https://github.com/samiyeahsan/quiz-html/blob/main/asset/quiz.hw4.png)
+
+[Deployed Link](https://samiyeahsan.github.io/quiz-html/)
---
+++ asset/script.js
@@ -0,0 +1,117 @@
+var quizContianer = document.querySelector("#quiz");
+var resultContianer = document.querySelector("#show-result");
+var startButton = document.querySelector("#start-quiz");
+var timerSec;
+var currQuestionIdx = 0;
+var winner;
+var timeCount = document.querySelector("#timer");
+var score = 0;
+var end = document.querySelector(".end");
+
+
+
+var question = [{
+  question: "Which city is the capital of Minnesota State?",
+  answers: ["Eagan", "Minneapolis", " St. Paul", "Moorhead"],
+  correctAnswer: "St. Paul"
+},
+{
+  question: "What color is the skye?",
+  answers: ["Black", "Brown", "Blue", "Pink"],
+  correctAnswer: "Blue"
+},
+{
+  question: "Which city is the Capital of United State of America?",
+  answers: ["Minneapolis", "Seattle", "Harrisburg", "Washington DC"],
+  correctAnswer: "Washington DC"
+},
+{
+  question: "Who is the instructor of this bootcamp?",
+  answers: ["Gary", "David", "Katy", "Ben"],
+  correctAnswer: "Gary"
+},
+{
+  question: "Where is the YellowStone national park located?",
+  answers: ["California", "Wyoming", "Minnesota", "Pennsylvania"],
+  correctAnswer: "Wyoming"
+}
+
+]
+
+function start() {
+  timerSec = 50;
+  var timeInterval = setInterval(function () {
+    timerSec--;
+    timeCount.textContent = timerSec;
+    if (timerSec <= 0) {
+      clearInterval(timeInterval)
+      quizEnd()
+    }
+  }, 1000);
+  displayQuestion();
+}
+
+function displayQuestion() {
+  quizContianer.innerHTML = ""
+  var currQuestion = question[currQuestionIdx]
+  var pTag = document.createElement("p");
+  pTag.textContent = currQuestion.question
+  quizContianer.appendChild(pTag)
+
+  for (var i = 0; i < currQuestion.answers.length; i++) {
+    var answer = currQuestion.answers[i]
+    var btn = document.createElement("button")
+    btn.textContent = answer
+    btn.saveValue = answer
+    btn.addEventListener("click", checkAnswers)
+
+    quizContianer.appendChild(btn)
+  }
+}
+
+function checkAnswers(event) {
+  console.log(event.target)
+  if (event.target.textContent === question[currQuestionIdx].correctAnswer) {
+    score++
+    console.log("correct")
+  } else {
+    timerSec -= 5
+    console.log("incorrect")
+  }
+  if (currQuestionIdx + 1 === question.length) {
+
+    quizEnd()
+  } else {
+    currQuestionIdx++
+    displayQuestion()
+  }
+}
+// function answerIsCorrect() {
+
+// }
+// function answerIsWrong() {
+
+// }
+function quizEnd() {
+  
+
+    end.textContent = "Your Score : " + score;
+    //end.style.display = "block"
+
+
+}
+
+/*question.addEventListener("click", function(event){
+ var currQuestion = question[0]
+ if( event.target.matches(button) )
+ if( event.target.textContent === currQuestion.correct){
+   answerIsCorrect()
+
+ }else {
+   answerIsWrong()
+ }
+ currQuestionIdx++
+   
+ })
+ */
+startButton.addEventListener("click", start);
